08a041c672fd1f080ce3953dac3183bf7cd3ee01
yusufva <yusufvalent@gmail.com>
2023-04-17 14:08:58 +0700

    adding unit test

--- service/product_service_test.go
+++ service/product_service_test.go
@@ -1,7 +1,9 @@
 package service
 
 import (
+	"net/http"
 	"testing"
+	"time"
 	"tugas-sesi12/entity"
 	"tugas-sesi12/pkg/errrs"
 	"tugas-sesi12/repository/product_repository"
@@ -12,13 +14,16 @@ import (
 func Test_ProductService_GetProductById_Success(t *testing.T) {
 	productRepo := product_repository.NewProductRepoMock()
 	productService := NewProductService(productRepo)
+	currentTime := time.Now()
 
 	product_repository.GetProductById = func(productId int) (*entity.Product, errrs.MessageErr) {
 		return &entity.Product{
 			Id:          1,
-			Title:       "buku belajar",
-			Description: "ini buku",
+			Title:       "Test Product",
+			Description: "Test Product Description",
 			UserId:      1,
+			CreatedAt:   currentTime,
+			UpdatedAt:   currentTime,
 		}, nil
 	}
 
@@ -27,7 +32,69 @@ func Test_ProductService_GetProductById_Success(t *testing.T) {
 	assert.Nil(t, err)
 	assert.NotNil(t, result)
 	assert.Equal(t, 1, result.Id)
-	assert.Equal(t, "buku belajar", result.Title)
-	assert.Equal(t, "ini buku", result.Description)
+	assert.Equal(t, "Test Product", result.Title)
+	assert.Equal(t, "Test Product Description", result.Description)
 	assert.Equal(t, 1, result.UserId)
+}
+
+func Test_ProductService_GetProductById_NotFoundError(t *testing.T) {
+	productRepo := product_repository.NewProductRepoMock()
+	productService := NewProductService(productRepo)
+
+	product_repository.GetProductById = func(productId int) (*entity.Product, errrs.MessageErr) {
+		return nil, errrs.NewNotFoundError("product data not found")
+	}
+
+	response, err := productService.GetProductById(1)
+
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+	assert.Equal(t, http.StatusNotFound, err.Status())
+	assert.Equal(t, "product data not found", err.Message())
+	assert.Equal(t, "NOT_FOUND", err.Error())
+}
+
+func Test_ProductService_GetAllProducts_Success(t *testing.T) {
+	productRepo := product_repository.NewProductRepoMock()
+	productService := NewProductService(productRepo)
+	currentTime := time.Now()
+
+	products := []*entity.Product{
+		{
+			Id:          1,
+			Title:       "Test Product 1",
+			Description: "Product 1 Test Description",
+			UserId:      1,
+			CreatedAt:   currentTime,
+			UpdatedAt:   currentTime,
+		},
+	}
+
+	product_repository.GetAllProducts = func() ([]*entity.Product, errrs.MessageErr) {
+		return products, nil
+	}
+
+	respponse, err := productService.GetAllProducts()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, respponse)
+	assert.Equal(t, 1, len(respponse.Data))
+	assert.Equal(t, "Test Product 1", respponse.Data[0].Title)
+	assert.Equal(t, http.StatusOK, respponse.StatusCode)
+	assert.Equal(t, "product data have been sent successfully", respponse.Message)
+}
+
+func Test_ProductService_GetAllProducts_NotFoundError(t *testing.T) {
+	productRepo := product_repository.NewProductRepoMock()
+	productService := NewProductService(productRepo)
+
+	product_repository.GetAllProducts = func() ([]*entity.Product, errrs.MessageErr) {
+		return []*entity.Product{}, nil
+	}
+
+	response, err := productService.GetAllProducts()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, response)
+	assert.Equal(t, 0, len(response.Data))
 }
